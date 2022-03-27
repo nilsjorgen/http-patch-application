ccd1b37224e66d42813db640547d769ccc85d51f
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-03-27 12:21:30 +0200

    Removed Spring inspection suppression

--- src/main/kotlin/no/njm/Application.kt
+++ src/main/kotlin/no/njm/Application.kt
@@ -6,7 +6,6 @@ import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.web.client.RestTemplate
 
-@Suppress("SpringJavaInjectionPointsAutowiringInspection")
 @SpringBootApplication
 class Application {
     /**
